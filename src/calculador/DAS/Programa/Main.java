4a615cc7d5396e65a60eb5fa619a70c8aa60b805
italo palhares <palharesitalo@hotmail.com>
2023-12-19 16:03:37 -0300

    criando classe rendas e loop para add na list

--- src/calculador/DAS/Programa/Main.java
+++ src/calculador/DAS/Programa/Main.java
@@ -1,5 +1,44 @@
 package calculador.DAS.Programa;
 
-public class Main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		List<Rendas> listaRendas = new ArrayList<Rendas>();
+		Scanner input = new Scanner(System.in);
+		System.out.println("Calculador DAS");
+		System.out.println("**************");
+		System.out.println("Seja bem vindo");
+		System.out.println("Esse é seu primeiro Acesso?");
+		System.out.println("Digite '1' para sim ou '2' para não");
+		int resposta = input.nextInt();
+
+		if (resposta == 1) {
+			int dozemeses = 12;
+			System.out.println("Então iremos te guiar no processo");
+			System.out.println("Vamos adicionar suas ultimas rendas ao sistema");
+			System.out.println("Lembre-se, adicionaremos os ultimos 12 meses, do mais antigo até o atual.");
+			for (int i = 1; i <= dozemeses; i++) {
+				System.out.println("Informe: ");
+				System.out.print("Mês: ");
+				String mes = input.next();
+				System.out.print("Valor: ");
+				double valor = input.nextDouble();
+				Rendas renda = new Rendas(mes, valor);
+				listaRendas.add(renda);
+				System.out.println("mes anterior: " + mes + " Valor anterior: " + valor);
+				System.out.println("Continue");
+
+			}
+			System.out.println("lista criada com sucesso, vamos checar: " + listaRendas);
+			input.close();
+
+			if (resposta == 2) {
+				System.out.println("Prossiga apara atualizar a renda atual");
+			}
+
+		}
+	}
 }
--- src/calculador/DAS/Programa/Rendas.java
+++ src/calculador/DAS/Programa/Rendas.java
@@ -1,7 +1,26 @@
 package calculador.DAS.Programa;
 
 public class Rendas {
-//poder checar,editar e add
 	
+	private String mes;
+	private double valor;
+	
+	public Rendas(String mes, double valor) {
+		this.mes = mes;
+		this.valor = valor;
+	}
+	
+	public String getMes() {
+		return mes;
+	}
+	public double getValor() {
+		return valor;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "mês: " + this.mes + " valor: " + this.valor;
+	}
 	
 }
